23700bfa5bfd4985b133447e5b5f3018575e5940
Damián Adams <contact@mianlabs.com>
2018-04-26 20:42:24 -0400

    Add ejs and basic express configurations

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose'),
 	express = require('express'),
 	app = express();
 
+mongoose.connect('mongodb://localhost/semantic_blog');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => res.send());
