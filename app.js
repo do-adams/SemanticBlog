b73ff5340a024b2ce7ec837fc8e7d15091799bfe
Damián Adams <contact@mianlabs.com>
2018-04-26 21:17:26 -0400

    Add mongoose and ejs, along with a blog schema and the blogs index view

--- app.js
+++ app.js
@@ -6,10 +6,31 @@ const mongoose = require('mongoose'),
 
 mongoose.connect('mongodb://localhost/semantic_blog');
 
+// APP CONFIG
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => res.send());
+// MONGOOSE MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+// RESTFUL ROUTES
+app.get('/', (req, res) => {
+	res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+	Blog.find({}, function(err, blogs) {
+		if (err) console.log(err);
+		else res.render('index', {blogs: blogs});
+	});
+});
 
 app.listen(3000, () => console.log('Server has started!'));
